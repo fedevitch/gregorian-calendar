0146a7ef81a25be9c8c80ba0c419362f76067b10
Lyubomyr Fedevitch <lmfedevitch@gmail.com>
2016-04-03 17:06:51 +0300

    added uk_UA locale

---
+++ src/locale/uk_UA.js
@@ -0,0 +1,11 @@
+/*
+ * uk-ua locale
+ * @ignore
+ * @author fedevitch@ukr.net
+ */
+module.exports = {
+  // in minutes
+  timezoneOffset: 2 * 60,
+  firstDayOfWeek: 1,
+  minimalDaysInFirstWeek: 1,
+};
